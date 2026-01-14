4cd71396ec5c02f56afaa164a10c5134c381b36c
“Nisha <nishayadav4245@gmail.com>
2026-01-14 23:08:23 +0530

    Fix ESLint unused variable in Footer

--- developerFolio-master/src/components/footer/Footer.js
+++ developerFolio-master/src/components/footer/Footer.js
@@ -1,21 +1,22 @@
 import React, {useContext} from "react";
 import "./Footer.scss";
 import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  useContext(StyleContext); // keep context if required globally
+
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-       <p>
+        <p>
           <a
             href="https://github.com/nishayadav4245"
             target="_blank"
             rel="noreferrer"
           >
-           
+            {/* Add text or icon here */}
+            GitHub
           </a>
         </p>
       </div>
